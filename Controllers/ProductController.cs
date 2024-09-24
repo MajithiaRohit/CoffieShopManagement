ee40667ff9adb0fc4325949523fa87bac186ebad
Rohit Majithia <23030501031@darshan.ac.in>
2024-09-24 23:16:30 +0530

    popup message updated

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -18,23 +18,6 @@ namespace CoffieShop.Controllers
         }
         #endregion
 
-        #region Display Product List
-        public IActionResult ProductList()
-        {
-            string? connectionString = this.configuration.GetConnectionString("ConnectionString");
-            SqlConnection connection = new SqlConnection(connectionString);
-            connection.Open();
-            SqlCommand command = connection.CreateCommand();
-            command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "SP_Products_SelectAll";
-            SqlDataReader reader = command.ExecuteReader();
-            DataTable table = new DataTable();
-            table.Load(reader);
-            return View(table);
-        }
-        #endregion
-
-
         public List<UserDropDownModel> setUserDropDown()
         {
             #region Display User by thir id DropDownList
@@ -53,139 +36,162 @@ namespace CoffieShop.Controllers
 
             foreach (DataRow dataRow in dataTable1.Rows)
             {
-                UserDropDownModel userDropDownModel = new UserDropDownModel();
-                userDropDownModel.UserID = Convert.ToInt32(dataRow["UserID"]);
-                userDropDownModel.UserName = dataRow["UserName"].ToString();
+                UserDropDownModel userDropDownModel = new UserDropDownModel
+                {
+                    UserID = Convert.ToInt32(dataRow["UserID"]),
+                    UserName = dataRow["UserName"].ToString()
+                };
                 users.Add(userDropDownModel);
             }
-
             return users;
             #endregion
 
         }
 
-
-
-        #region AddEditProduct And Set textbox feild
-        public IActionResult AddEditProduct(int ProductID)
+        #region Display Product List
+        public IActionResult ProductList()
         {
             string? connectionString = this.configuration.GetConnectionString("ConnectionString");
-            ViewBag.UserList = setUserDropDown();
-
-            #region Display ProductByID
             SqlConnection connection = new SqlConnection(connectionString);
             connection.Open();
             SqlCommand command = connection.CreateCommand();
             command.CommandType = CommandType.StoredProcedure;
-            command.CommandText = "SP_Products_SelectByPK";
-            command.Parameters.AddWithValue("@ProductID", ProductID);
+            command.CommandText = "SP_Products_SelectAll";
             SqlDataReader reader = command.ExecuteReader();
             DataTable table = new DataTable();
             table.Load(reader);
-            ProductModel productModel = new ProductModel();
-
-            foreach (DataRow dataRow in table.Rows)
-            {
-                productModel.ProductID = Convert.ToInt32(@dataRow["ProductID"]);
-                productModel.ProductName = @dataRow["ProductName"].ToString();
-                productModel.ProductCode = @dataRow["ProductCode"].ToString();
-                productModel.ProductPrice = Convert.ToDecimal(@dataRow["ProductPrice"]);
-                productModel.Description = @dataRow["Description"].ToString();
-                productModel.UserID = Convert.ToInt32(@dataRow["UserID"]);
-            }
-
-            #endregion
-
-            return View(productModel);
+            return View(table);
         }
         #endregion
 
-        #region saveAddEditProduct
-        public IActionResult SaveProduct(ProductModel product)
+        #region AddEditProduct And Set textbox feild
+        public IActionResult AddEditProduct(int ProductID)
         {
-            string? connectionString = this.configuration.GetConnectionString("ConnectionString");
-       
-            #region saveProduct
-            if (ModelState.IsValid)
-            {
-                try
-                {
+          
+            ViewBag.UserList = setUserDropDown();
 
-                    SqlConnection connection = new SqlConnection(connectionString);
-                    
-                    connection.Open();
-                    SqlCommand command = connection.CreateCommand();
-                        
-                    command.CommandType = CommandType.StoredProcedure;
-
-                    if (product.ProductID == null)
-                    {
-                        command.CommandText = "SP_Products_Insert";
-                    }
-                    else
-                    {
-                        command.CommandText = "SP_Products_UpdateByPK";
-                        command.Parameters.Add("@ProductID", SqlDbType.Int).Value = product.ProductID;
-                    }
-
-                    // Add Parameters
-                    command.Parameters.Add("@ProductName", SqlDbType.VarChar).Value = product.ProductName;
-                    command.Parameters.Add("@ProductPrice", SqlDbType.Decimal).Value = product.ProductPrice;
-                    command.Parameters.Add("@ProductCode", SqlDbType.VarChar).Value = product.ProductCode;
-                    command.Parameters.Add("@Description", SqlDbType.VarChar).Value = product.Description;
-                    command.Parameters.Add("@UserID", SqlDbType.Int).Value = product.UserID;
-
-                    // Execute the command
-                    command.ExecuteNonQuery();
-                        
-                    
-                    TempData["SuccessMessageAdd"] = "Product added successfully!";
-                    TempData["SuccessMessageEdit"] = "Product Edit  successfully!";
-                    TempData["ProductID"] = product.ProductID; 
-                    return RedirectToAction("ProductList");
-                }
-                catch (Exception ex)
-                {
-                   
-                    Console.WriteLine("An error occurred: " + ex.Message);
-
-                    TempData["Error"] = ex;
-                    return View("AddEditProduct", product);
-                }
-            }
-            else
-            {
-                ViewBag.UserList = setUserDropDown();
-                return View("AddEditProduct", product);
-            }
-            #endregion
-        }
-
-        #endregion
-
-
-        #region DeleteProduct
-        public IActionResult DeleteProduct(int ProductID) {
-            try
+            
+            if (ProductID > 0)
             {
                 string? connectionString = this.configuration.GetConnectionString("ConnectionString");
                 SqlConnection connection = new SqlConnection(connectionString);
                 connection.Open();
                 SqlCommand command = connection.CreateCommand();
                 command.CommandType = CommandType.StoredProcedure;
-                command.CommandText = "SP_Products_DeleteByPK";
-                // Add the UserID parameter
-                command.Parameters.Add("@ProductID", SqlDbType.Int).Value = ProductID;
-                // Execute the command
-                command.ExecuteNonQuery();
-                return RedirectToAction("ProductList");
+                command.CommandText = "SP_Products_SelectByPK";
+                command.Parameters.AddWithValue("@ProductID", ProductID);
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+                connection.Close(); 
+
+                ProductModel productModel = new ProductModel();
+
+                foreach (DataRow dataRow in table.Rows)
+                {
+                    productModel.ProductID = Convert.ToInt32(dataRow["ProductID"]);
+                    productModel.ProductName = dataRow["ProductName"].ToString();
+                    productModel.ProductCode = dataRow["ProductCode"].ToString();
+                    productModel.ProductPrice = Convert.ToDecimal(dataRow["ProductPrice"]);
+                    productModel.Description = dataRow["Description"].ToString();
+                    productModel.UserID = Convert.ToInt32(dataRow["UserID"]); 
+                }
+
+                return View(productModel);
             }
-            catch (Exception ex) {
-              
-                TempData["errorEx"] = "Can not Delete.";
-                return RedirectToAction("ProductList");
+            else
+            {
+                return View(new ProductModel());
             }
         }
+
         #endregion
+
+        #region saveAddEditProduct
+        public IActionResult SaveProduct(ProductModel product)
+        {
+            string? connectionString = this.configuration.GetConnectionString("ConnectionString");
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    if (product.ProductID == null) 
+                    {
+                        command.CommandText = "SP_Products_Insert";
+                    }
+                    else 
+                    {
+                        command.CommandText = "SP_Products_UpdateByPK";
+                        command.Parameters.AddWithValue("@ProductID", product.ProductID);
+                    }
+
+                    command.Parameters.AddWithValue("@ProductName", product.ProductName);
+                    command.Parameters.AddWithValue("@ProductPrice", product.ProductPrice);
+                    command.Parameters.AddWithValue("@ProductCode", product.ProductCode);
+                    command.Parameters.AddWithValue("@Description", product.Description);
+                    command.Parameters.AddWithValue("@UserID", product.UserID);
+
+                    command.ExecuteNonQuery();
+
+                    connection.Close();
+
+                    if (product.ProductID == null)
+                        TempData["SuccessMessageAdd"] = "Product added successfully!";
+                    else
+                        TempData["SuccessMessageEdit"] = "Product edited successfully!";
+
+                    return RedirectToAction("ProductList");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred: " + ex.Message);
+                    TempData["Error"] = ex.Message;
+                    return View("AddEditProduct", product);
+                }
+            }
+            else
+            {
+                ViewBag.UserList = setUserDropDown();
+                return View("AddEditProduct", product); 
+            }
+        }
+
+
+        #endregion
+
+
+        #region DeleteProduct
+        public IActionResult DeleteProduct(int ProductID)
+        {
+            try
+            {
+                string? connectionString = this.configuration.GetConnectionString("ConnectionString");
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "SP_Products_DeleteByPK";
+                    command.Parameters.Add("@ProductID", SqlDbType.Int).Value = ProductID;
+
+                    command.ExecuteNonQuery();
+                }
+                TempData["SuccessMessageDelete"] = "Product deleted successfully!";
+            }
+            catch (Exception ex)
+            {
+                TempData["errorEx"] = "Cannot delete product.";
+            }
+
+            return RedirectToAction("ProductList");
+        }
+        #endregion
+
     }
 }
